b5b295659f3d40216d0980a9bd904bbd120e2e90
purpledmango <nitishmerlin@gmail.com>
2024-11-16 16:22:05 +0530

    Major upgrade 1.0.1

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-$17ut0(g*j(yq2$ohfpqbk2mkgw8@ei$b8q3!+*ynvyv7t3afx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
